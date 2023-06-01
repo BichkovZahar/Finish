1b34f9fd5265afe6c9860db6ca8eac245a0337f3
Asus <Bichk_di78@student.itstep.org>
2023-06-01 19:38:54 +0300

    5

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-a = input("Введіть рядок: ")
-for i in a:
-    print(i)
+def palindrom(func):
+    if func == func[::-1]:
+        print(func)
+        print("Рядок є паліндромом")
+    else:
+        print(func)
+        print("Рядок не є паліндромом")
+palindrom("fwf")
