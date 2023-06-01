50b3014a3dae7602184bd362b7a51200ed630330
Asus <Bichk_di78@student.itstep.org>
2023-06-01 19:47:28 +0300

    6

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def palindrom(func):
-    if func == func[::-1]:
-        print(func)
-        print("Рядок є паліндромом")
-    else:
-        print(func)
-        print("Рядок не є паліндромом")
-palindrom("fwf")
+def filter_even_numbers(numbers):
+    even_numbers = [num for num in numbers if num % 2 == 0]
+    return even_numbers
+
+# Вихідний список цілих чисел
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Виклик функції для фільтрації парних чисел
+even_numbers = filter_even_numbers(numbers)
+
+# Виведення нового списку, що містить парні числа
+print(even_numbers)
