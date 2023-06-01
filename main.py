a89984b4c559eefd262d993c8d8a236821a92d81
Asus <Bichk_di78@student.itstep.org>
2023-06-01 20:40:44 +0300

    9

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def max1(grades):
-    max_grade = max(grades.values())
-    max_grade = [student for student, grade in grades.items() if grade == max_grade]
-    return max_grade[0]
-grades = {}
-for i in range(3):
-    student = input(f"Введіть імя: ")
-    grade = input(f"Введіть оцінку: ")
-    grades[student] = grade
-print("Студент з найвищою оцінкою:", max1(grades))
-
+def python(strings):
+    item = [s for s in strings if "Python" in s]
+    return item
+dict = []
+lst = input("Введіть рядок: ")
+dict.append(lst)
+finish = python(dict)
+print(finish)
